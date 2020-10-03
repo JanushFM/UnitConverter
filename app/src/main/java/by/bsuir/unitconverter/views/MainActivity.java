42018562cecc551f00f744ce4f07f3e72a92f712
JanushFM <janusz.fiaskou@gmail.com>
2020-10-03 16:43:34 +0300

    click on navigation item works

--- app/src/main/java/by/bsuir/unitconverter/views/MainActivity.java
+++ app/src/main/java/by/bsuir/unitconverter/views/MainActivity.java
@@ -1,16 +1,68 @@
 package by.bsuir.unitconverter.views;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 import by.bsuir.unitconverter.R;
+import by.bsuir.unitconverter.view_model.SharedViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private SharedViewModel model;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        model = new ViewModelProvider(this).get(SharedViewModel.class);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.drawer_open, R.string.drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        model.getToolbarTitleResourceID().observe(this, titleResourceID -> toolbar.setTitle(getString(titleResourceID)));
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        model.selectUnitListAndToolbarTitle(item.getItemId());
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
