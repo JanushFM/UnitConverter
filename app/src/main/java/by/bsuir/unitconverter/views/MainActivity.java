f6fcc1462300155de918e6db8dc8085a78d3586d
JanushFM <janusz.fiaskou@gmail.com>
2020-10-03 15:57:36 +0300

    added navigation drawer

--- app/src/main/java/by/bsuir/unitconverter/views/MainActivity.java
+++ app/src/main/java/by/bsuir/unitconverter/views/MainActivity.java
@@ -1,8 +1,10 @@
-package by.bsuir.unitconverter;
+package by.bsuir.unitconverter.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import by.bsuir.unitconverter.R;
 
 public class MainActivity extends AppCompatActivity {
 
